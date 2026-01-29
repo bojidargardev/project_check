013564ce91e7ea2df5701d5d789b56bff53c5e77
bojidargardev <bojidar.gardev@gmail.com>
2026-01-29 09:45:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,28 @@
 import streamlit as st
 
-st.title("Мини тест – 3 проверки")
+st.title("Мини тест – 2 проверки")
 
-love_python = st.radio(
+answer_yes_no = st.radio(
     "Обичаш ли Python?",
     ("да", "не")
 )
 
-number = st.number_input("Въведи число", step=1)
-
-answer = st.number_input("Колко е 5 × 5?", step=1)
+answer_math = st.number_input("Колко е 5 × 5?", step=1)
 
 if st.button("Провери"):
     
     points = 0
 
-    if love_python == "да":
+    if answer_yes_no == "да":
         st.success("Верен отговор")
         points += 1
     else:
         st.error("Грешен отговор")
 
-    if number > 10:
+    if answer_math == 25:
         st.success("Верен отговор")
         points += 1
     else:
         st.error("Грешен отговор")
 
-    if answer == 25:
-        st.success("Верен отговор")
-        points += 1
-    else:
-        st.error("Грешен отговор")
-
-    st.write("Резултат:", points, "/ 3")
+    st.write("Резултат:", points, "/ 2")
